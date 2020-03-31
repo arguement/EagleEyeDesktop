48f1e977f35e7b1ea9d0ae7de01b72470b9087b1
Brittany Thomas <bolivthom@outlook.com>
2020-03-31 15:06:03 -0500

    connect sidebar view to page view for home and report page

---
+++ src/renderer/components/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <main>
+        <homeSidebar></homeSidebar>
+    </main>
+  </div>
+</template>
+
+<script>
+  import reportsidebar from './sidebar/homeSidebar'
+
+  export default {
+    name: 'home-page',
+    components: { homeSidebar},
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+
+</style>
---
+++ src/renderer/components/ReportPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <main>
+        <reportSidebar></reportSidebar>
+    </main>
+  </div>
+</template>
+
+<script>
+  import reportsidebar from './sidebar/reportSidebar'
+
+  export default {
+    name: 'report-page',
+    components: { reportSidebar},
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+
+</style>
