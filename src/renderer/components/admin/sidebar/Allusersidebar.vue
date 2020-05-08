34f41698091f08203c5c8cab2bd199fdf1802a06
Brittany Thomas <bolivthom@outlook.com>
2020-05-07 21:00:53 -0500

    user probs

--- src/renderer/components/admin/sidebar/Allusersidebar.vue
+++ src/renderer/components/admin/sidebar/Allusersidebar.vue
@@ -25,7 +25,7 @@
       </li>
      
       <li id="tasks-link" class="nav-item">
-        <router-link to="/allusers" class="nav-link" id="dashboard-link-user">ALL USERS</router-link>
+        <router-link to="/allusers" class="nav-link" id="dashboard-link-user">USERS</router-link>
       </li>
       
       <li id="log-out" class="nav-item">
