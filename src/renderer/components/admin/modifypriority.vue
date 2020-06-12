2ada83cbef2d531e3465ab30b5880f784213f513
System Administrator <root@deans-MacBook-Air.local>
2020-06-11 21:48:54 -0500

    change

---
+++ src/renderer/components/admin/modifypriority.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <main>
+            <div id="gaza-content">
+              <prioritySidebar></prioritySidebar>
+              <ModifyPriority></ModifyPriority>
+            </div>
+        </main>   
+    </div>
+</template> 
+<script>
+import prioritySidebar from './sidebar/prioritySidebar'
+import ModifyPriority from './content/modifyprioritycontent' 
+export default { 
+    name: 'ModifyPrioritypage',
+    components: { ModifyPriority, prioritySidebar},
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+    
+}
+</script>  
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #report-page {
+    background-color: #F8F9F9;
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height:100%;
+  }
+  
+</style>
