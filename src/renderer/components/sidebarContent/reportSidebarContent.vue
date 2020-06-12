79dc540abcebb11124e17a50f5b37aeba4b008dd
System Administrator <root@deans-MacBook-Air.local>
2020-06-11 20:31:29 -0500

    edit

--- src/renderer/components/sidebarContent/reportSidebarContent.vue
+++ src/renderer/components/sidebarContent/reportSidebarContent.vue
@@ -374,7 +374,7 @@ export default {
        setInterval(() => { // used to pll the database to see if there is any new reports
          
          
-         db.collection("Crime Report").get()
+         /*db.collection("Crime Report").get()
         .then(snapshot => {
           snapshot.forEach(doc => {
             
@@ -393,7 +393,7 @@ export default {
            console.log("they not the same")
          }
          this.reports2=[]
-       },30000); 
+       },30000); */
        
     },
   }
